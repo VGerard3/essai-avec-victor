be5b3670ec578f7ac33bcbd76a1365b0c09ce687
Titor07 <dumange.victor@gmail.com>
2022-06-21 08:42:13 +0200

    Hello World

---
+++ module.c
@@ -0,0 +1,6 @@
+#include "module.h"
+#include<stdio.h>
+
+void print_Hello(){
+    printf("Hello World !");
+}
---
+++ premier_programme_partage.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"module.h"
+
+int main(){
+    print_Hello();
+}
